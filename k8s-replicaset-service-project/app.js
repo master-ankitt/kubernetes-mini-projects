1d2886baa8ba83b8a902ce1610dda4d7e1883eb5
Ankit <125477559+master-ankitt@users.noreply.github.com>
2025-11-20 21:27:33 +0530

    Create app.js

---
+++ k8s-replicaset-service-project/app.js
@@ -0,0 +1,3 @@
+document.getElementById("pod").textContent = window.location.hostname;
+document.getElementById("node").textContent = "Kubernetes Node";
+document.getElementById("time").textContent = new Date().toLocaleString();
